91516e13431dc242cae84d222f4f8dc33e62c491
phingeiam <phingeiam@gmail.com>
2024-06-23 10:03:53 +0700

    first commit

---
+++ demoGithubproject/demoGithubproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoGithubproject
+//
+//  Created by phing eiam on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoGithubproject/demoGithubproject/demoGithubprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGithubprojectApp.swift
+//  demoGithubproject
+//
+//  Created by phing eiam on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demoGithubprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
